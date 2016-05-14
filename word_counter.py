2e028c3b43de2a4d2c2040a8325064c214838656
Deshawn Nelson <deshawn.a.nelson@gmail.com>
2016-05-14 18:06:29 -0400

    Updates Python Script: Word Counter

--- word_counter.py
+++ word_counter.py
@@ -4,6 +4,7 @@ word_count = 0
 
 def main():
 	global word_count
+	word_count = 0
 	print 'Please enter a String or filename'
 	print 'If the file isn\'t in the same directory as the program, be sure to enter the full path! (e.g. C:/xxxx.txt, or E:/xxxxxxx/xxxx.txt'
 	string = raw_input()
@@ -19,13 +20,41 @@ def main():
 
 	print 'There are ' + str(word_count) + ' words in that string.'
 
+	reset()
+
+
 def read_file(file):
 	global word_count
+	word_count = 0
 	f = open(file, 'r')
 
+	for line in f:
+		print line
+		
+	f.close()
+
+	f = open(file, 'r')
 	for word in f.read().split():
 		word_count += 1
 
 	print 'There are ' + str(word_count) + ' words in that file.'
+	f.close()
+
+	reset()
+
+def reset():
+
+	print 'Would you like to count another string? (Press y or n)'
+
+	user_input = raw_input()
+
+	if user_input == 'y':
+		main()
+	elif user_input == 'n':
+		print 'Thank you for trying my program, Goodbye!'
+		quit()
+	else:
+		print 'Invalid input!'
+		reset()
 
 main()
