81418376e60af81db0e3fe3ddfa84d49e01eb79a
Deshawn Nelson <deshawn.a.nelson@gmail.com>
2016-05-14 14:16:01 -0400

    Adds Python Script: String Reverser

---
+++ string-reverser.py
@@ -0,0 +1,39 @@
+string = ''
+
+def intro():
+	'''
+	Prints an introduction message to the screen, takes in user input and assigns it to the global string function, 
+	and then calls the string reversing function.
+	'''
+	global string
+	print 'Welcome to my string reverser program'
+	print 'With this program you can reverse any string you\'ve entered in the prompt'
+	string = raw_input('So Please enter the string you want to reverse: ')
+
+	str_rev()
+
+
+def str_rev():
+	'''
+	Pulls the string
+	'''
+	global string
+	rev_string = string[::-1]
+	print rev_string
+	reset()
+
+
+def reset():
+	cont = raw_input('Would you like to reverse another string? (Press y or n): ').lower()
+
+	if cont == 'y':
+		intro()
+	elif cont == 'n':
+		print 'Thanks you for running my program, Goodbye!'
+		quit()
+	else:
+		print 'Invalid selection!'
+		reset()
+
+
+intro()
--- tax-calc.py
+++ tax-calc.py
@@ -1,8 +1,8 @@
-price = int(input('Please enter the price of the item: '))
+price = float(input('Please enter the price of the item: '))
 
 tax = float(input('Please enter your local sales tax: '))
 
 total = ((tax / 100) * price) + price
 
 
-print('Your item\'s price is:',round(total, 2))
+print('Your item\'s price is: $' + str(round(total, 2)))
